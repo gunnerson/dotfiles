91c1bcc6b6f46ff61a88e259dee9e739c15fc69c
rumata <rawpluganchor@gmail.com>
2024-11-14 19:22:06 -0600

    Update 2024-11-14

---
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,29 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'personal',
+        path = '/mnt/bnas/docs/vaults/personal',
+      },
+    },
+    notes_subdir = 'inbox',
+    new_notes_location = 'notes_subdir',
+    disable_frontmatter = true,
+    templates = {
+      subdir = 'templates',
+      date_format = '%Y-%m-%d',
+      time_format = '%H:%M:%S',
+    },
+    completion = {
+      nvim_cmp = false,
+      min_chars = 2,
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/python-lsp.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    -- https://github.com/mfussenegger/nvim-dap-python
+    'mfussenegger/nvim-dap-python',
+    ft = 'python',
+    dependencies = {
+      -- https://github.com/mfussenegger/nvim-dap
+      'mfussenegger/nvim-dap',
+      'rcarriga/nvim-dap-ui',
+    },
+    config = function()
+      -- Update the path passed to setup to point to your system or virtual env python binary
+      require('dap-python').setup '/usr/bin/python'
+    end,
+  },
+  {
+    'psf/black',
+    ft = 'python',
+    config = function()
+      -- Automatically format file buffer when saving
+      vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+        pattern = '*.py',
+        callback = function()
+          vim.cmd 'Black'
+        end,
+      })
+    end,
+  },
+  {
+    'nvimtools/none-ls.nvim',
+    config = function()
+      require('null-ls').setup {
+        sources = {
+          require('null-ls').builtins.formatting.black,
+          require('null-ls').builtins.diagnostics.mypy.with {
+            extra_args = function()
+              local virtual = os.getenv 'VIRTUAL_ENV' or os.getenv 'CONDA_PREFIX' or '/usr'
+              return { '--python-executable', virtual .. '/bin/python' }
+            end,
+          },
+        },
+      }
+    end,
+    requires = { 'nvim-lua/plenary.nvim' },
+  },
+}
