67b0c9f28608d87d920a2ab36835a7bfc847b629
rumata <rawpluganchor@gmail.com>
2025-01-02 16:31:00 -0600

    Loose bins

--- bins/.local/share/bin/mnt-bnas.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo mount -t cifs //bnas.bwaargh.lan/docs/v /mnt/bnas/docs -o credentials=/etc/samba/credentials/bnas,uid=1000,gid=1000
-sudo mount -t cifs //bnas.bwaargh.lan/media /mnt/bnas/media -o credentials=/etc/samba/credentials/bnas,uid=1000,gid=1000
-sudo mount -t cifs //bnas.bwaargh.lan/public /mnt/bnas/public -o credentials=/etc/samba/credentials/bnas,uid=1000,gid=1000
-sudo mount -t cifs //bnas.bwaargh.lan/rpc /mnt/bnas/rpc -o credentials=/etc/samba/credentials/bnas,uid=1000,gid=1000
