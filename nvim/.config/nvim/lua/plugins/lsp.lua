ed3d64327a7d553f9463459141b510a9ab0b40f7
gunnerson <rawpluganchor@gmail.com>
2026-04-22 10:02:13 -0500

    Update 2026-04-22

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -37,7 +37,7 @@ return {
         map("<leader>cj", "<cmd>lua vim.diagnostic.open_float()<CR>", "Show diagnostic in float")
         map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+        if client and client:supports_method("textDocument/documentHighlight", event.buf) then
           local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
           vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
             buffer = event.buf,
